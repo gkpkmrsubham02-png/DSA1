bb26f735540784517df4f140b27bca89dbc584f1
Subham Gupta <gkpkmrsubham02@gmail.com>
2026-08-05 21:28:24 +0530

    Basic cpp

---
+++ tutorial1.cpp
@@ -0,0 +1,43 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int radius;
+//     cout<<"Enter radius:";
+//     cin>>radius;
+//     float area=3.14*radius*radius;
+//     cout<<"The area of circle is:"<<area;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int p,q;
+//     cout<<"Enter the value of p and q: ";
+//     cin>>p>>q;
+//     cout<<"p="<<p<<"q="<<q;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=3;
+//     cout<<(x=7);
+//     return 0;
+// }
+
+//Take integer as input and print half of the number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter your number: ";
+    cin>>x;
+    float y=(float)x;
+    cout<<y/2;
+    
+}
+
+//Take float input and print the fractional part of yhe real number
